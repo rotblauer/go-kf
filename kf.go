387306923b09ef31ddc08f8c643c1c1aa30c3fb7
ia <isaac.ardis@gmail.com>
2018-05-31 02:13:30 +0200

    damn kv

--- kf.go
+++ kf.go
@@ -8,19 +8,26 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
+	"strings"
+
+	bolt "github.com/coreos/bbolt"
 )
 
 // StoreConfig configures a new Store.
 type StoreConfig struct {
 	BaseDir string
 	Locking bool
+	KV      bool
 }
 
 // Store is the root directory in which to keep the key-file store.
 type Store struct {
-	baseDir string
-	locking bool
-	locked  bool
+	baseDir  string
+	locking  bool
+	locked   bool
+	kv       bool
+	db       *bolt.DB
+	kvBucket []byte
 }
 
 // NewStore creates a new storage instance.
@@ -37,7 +44,25 @@ func NewStore(c *StoreConfig) (*Store, error) {
 	if e != nil {
 		return nil, e
 	}
-	s := &Store{baseDir: baseDir, locking: c.Locking}
+	s := &Store{baseDir: baseDir, locking: c.Locking, kv: c.KV}
+
+	// getting too many open files error, turnin to darkside
+	if s.kv {
+		db, e := bolt.Open(s.join(".goddamnboltdatabasedontnameyourkeysthis"), os.ModePerm, nil)
+		if e != nil {
+			return nil, e
+		}
+		s.db = db
+		s.kvBucket = []byte("data")
+		if e := s.db.Update(func(tx *bolt.Tx) error {
+			_, e := tx.CreateBucketIfNotExists(s.kvBucket)
+			return e
+		}); e != nil {
+			return nil, e
+		}
+		return s, nil
+	}
+
 	f, e := os.Open(baseDir)
 
 	// initialize new dir
@@ -62,6 +87,21 @@ func NewStore(c *StoreConfig) (*Store, error) {
 
 // Set saves data.
 func (s *Store) Set(key string, value []byte) (err error) {
+	if s.kv {
+		keys := strings.Split(key, string(os.PathSeparator))
+		return s.db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket(s.kvBucket)
+			if len(keys) > 1 {
+				for i, k := range keys {
+					if i != len(keys)-1 {
+						b, _ = b.CreateBucketIfNotExists([]byte(k))
+					}
+				}
+			}
+			return b.Put([]byte(keys[len(keys)-1]), value)
+		})
+	}
+
 	for s.locking && s.locked {
 	}
 	if s.locking {
@@ -77,6 +117,26 @@ func (s *Store) Set(key string, value []byte) (err error) {
 
 // GetValue gets data from a key store.
 func (s *Store) GetValue(key string) (value []byte, err error) {
+	if s.kv {
+		keys := strings.Split(key, string(os.PathSeparator))
+		err = s.db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket(s.kvBucket)
+			if len(keys) > 1 {
+				for i, k := range keys {
+					if i != len(keys)-1 {
+						b = b.Bucket([]byte(k))
+						if b == nil {
+							return errors.New("bucket does not exist")
+						}
+					}
+				}
+			}
+			value = b.Get([]byte(keys[len(keys)-1]))
+			return nil
+		})
+		return value, err
+	}
+
 	for s.locking && s.locked {
 	}
 	if s.locking {
@@ -89,6 +149,24 @@ func (s *Store) GetValue(key string) (value []byte, err error) {
 
 // GetKeys gets all keys under a key store.
 func (s *Store) GetKeys(bucket string) (keys []string, err error) {
+	if s.kv {
+		keys := strings.Split(bucket, string(os.PathSeparator))
+		err = s.db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket(s.kvBucket)
+			for _, k := range keys {
+				b = b.Bucket([]byte(k))
+				if b == nil {
+					return errors.New("bucket does not exist")
+				}
+			}
+			return b.ForEach(func(k []byte, v []byte) error {
+				keys = append(keys, string(k))
+				return nil
+			})
+		})
+		return keys, err
+	}
+
 	for s.locking && s.locked {
 	}
 	if s.locking {
@@ -99,18 +177,57 @@ func (s *Store) GetKeys(bucket string) (keys []string, err error) {
 	if !existsDir(bucket) {
 		return nil, errors.New("uninitialized bucket")
 	}
-	fs, err := ioutil.ReadDir(bucket)
+	filepath.Walk(bucket, func(p string, f os.FileInfo, e error) error {
+		if e != nil {
+			err = e
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		keys = append(keys, f.Name())
+		return nil
+	})
+	// fs, err := ioutil.ReadDir(bucket)
 	if err != nil {
 		return nil, err
 	}
-	for _, f := range fs {
-		keys = append(keys, filepath.Base(f.Name()))
-	}
+	// for _, f := range fs {
+	// 	keys = append(keys, filepath.Base(f.Name()))
+	// }
 	return keys, err
 }
 
 // Delete deletes data.
+// TODO. Handle deleting buckets, too.
 func (s *Store) Delete(key string) (err error) {
+	if s.kv {
+		delBucket := strings.HasSuffix(key, string(os.PathSeparator))
+		keys := strings.Split(key, string(os.PathSeparator))
+		return s.db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket(s.kvBucket)
+			if len(keys) > 1 {
+				for i, k := range keys {
+					if !delBucket && i != len(keys)-1 {
+						b = b.Bucket([]byte(k))
+						if b == nil {
+							return errors.New("uninitialized bucket")
+						}
+					} else {
+						b = b.Bucket([]byte(k))
+						if b == nil {
+							return errors.New("uninitialized bucket")
+						}
+					}
+				}
+			}
+			if delBucket {
+				return tx.DeleteBucket([]byte(keys[len(keys)-1]))
+			}
+			return b.Delete([]byte(keys[len(keys)-1]))
+		})
+	}
+
 	for s.locking && s.locked {
 	}
 	if s.locking {
